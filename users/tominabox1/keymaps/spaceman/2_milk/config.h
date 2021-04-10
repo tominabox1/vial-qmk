13f18e4cee02879e38c2e24082b16c44e34217dc
TJ Campie <tom.campie@gmail.com>
2021-04-10 14:42:41 -0500

    fix milk

--- users/tominabox1/keymaps/spaceman/2_milk/config.h
+++ users/tominabox1/keymaps/spaceman/2_milk/config.h
@@ -1,12 +1,12 @@
 #undef DIRECT_PINS
 #define DIRECT_PINS { \
-    {B3}, \
-    {B7}  \
+    {D0}, \
+    {C6}  \
 }
 
 #ifdef RGBLIGHT_ENABLE
 #undef RGB_DI_PIN
-#define RGB_DI_PIN C7
+#define RGB_DI_PIN F5
 #undef RGBLED_NUM
 #define RGBLED_NUM 3
 #define RGBLIGHT_ANIMATIONS
--- users/tominabox1/keymaps/spaceman/2_milk/keymap.c
+++ users/tominabox1/keymaps/spaceman/2_milk/keymap.c
@@ -6,3 +6,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           RGB_MOD
           )
 };
+
+void matrix_init_kb(void) {
+
+   DDRD &= ~(1<<5);
+   PORTD &= ~(1<<5);
+
+   DDRB &= ~(1<<0);
+   PORTB &= ~(1<<0);
+};
