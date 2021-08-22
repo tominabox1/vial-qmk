e7464bede3b75d39892b6c132c83c8a8a7f25088
TJ Campie <tom.campie@gmail.com>
2021-08-22 12:05:11 -0500

    update _33 for split bars again

--- users/tominabox1/keymaps/underscore33/rev2/keymap.c
+++ users/tominabox1/keymaps/underscore33/rev2/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
 	_______________HOMEROW_MOD_________________,
 	_______________COLEMAK3____________________,
-    DM_REC1, KC_NAV_ENT, KC_NUM_SPC, KC_NO,KC_NAV_ENT, RGUI(RSFT(RCTL(KC_4)))
+    DM_REC1, KC_NO, KC_NAV_ENT, KC_NUM_SPC, KC_NAV_ENT, RGUI(RSFT(RCTL(KC_4)))
   ),
 
   [_NUM_SYM] = LAYOUT_33_split_spacewrap(
@@ -18,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NAV] = LAYOUT_33_split_spacewrap(
-    RESET,  KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO,  KC_PGDN,    KC_UP,  KC_PGUP,    TD(TD_RESET),
-    RGB_TOG,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
+    KC_PGDN,  KC_UP,  KC_PGUP, KC_NO, KC_NO,  KC_NO,  KC_PGDN,    KC_UP,  KC_PGUP,    TD(TD_RESET),
+    KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_NO,  KC_NO,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
     RGB_MOD,  DM_REC1,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_COMM,   KC_DOT,  KC_BSLS,
     RGB_MOD, KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS, RGUI(RSFT(KC_S))
   ),
