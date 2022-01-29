dd7df1f0457a589a403b4fe9c1b7767b1301a622
TJ Campie <tom.campie@gmail.com>
2022-01-29 14:27:08 -0600

    complete vial

--- keyboards/tominabox1/bigboy/keymaps/vial/config.h
+++ keyboards/tominabox1/bigboy/keymaps/vial/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 LAZYDESIGNERS
+/* Copyright 2022 tominabox1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,21 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
+#pragma once
 
-#define LT1_SPC LT(1, KC_SPC)
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_1,      KC_2,    KC_3,     KC_MUTE,
-        KC_4,      KC_5,    MO(1)
-        ),
-    [1] = LAYOUT(
-        RESET,     KC_NO,   KC_NO,   KC_NO,
-        KC_NO,     KC_NO,   KC_NO
-        ),
-    [2] = LAYOUT(
-        KC_NO,     KC_NO,   KC_NO,   KC_NO,
-        KC_NO,     KC_NO,   KC_NO
-        ),
-};
+#define VIAL_KEYBOARD_UID {0xF3, 0x17, 0xF0, 0xE1, 0x4C, 0xE8, 0x9E, 0x88}
