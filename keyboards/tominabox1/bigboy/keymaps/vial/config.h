04a9d9d1a56b068b2fdb0118cd846d8a0c33f96d
TJ <tom.campie@gmail.com>
2022-04-28 20:27:55 -0500

    fix pins

--- keyboards/tominabox1/bigboy/keymaps/vial/config.h
+++ keyboards/tominabox1/bigboy/keymaps/vial/config.h
@@ -1,4 +1,4 @@
-git/* Copyright 2022 tominabox1
+/* Copyright 2022 tominabox1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
