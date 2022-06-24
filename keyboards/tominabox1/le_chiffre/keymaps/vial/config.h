e17c7e1faa20ae52463e341d19bf407479ffe521
TJ Campie <tom.campie@gmail.com>
2022-06-24 08:51:42 -0500

    updates

--- keyboards/tominabox1/le_chiffre/keymaps/vial/config.h
+++ keyboards/tominabox1/le_chiffre/keymaps/vial/config.h
@@ -4,6 +4,9 @@
 
 #define VIAL_KEYBOARD_UID {0x81, 0x63, 0x69, 0xAD, 0xAA, 0x9D, 0xC7, 0x6F}
 
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
@@ -11,5 +14,5 @@
 #define RGBLIGHT_LIMIT_VAL 150
 
 #undef COMBO_COUNT
-#define VIAL_COMBO_ENTRIES 9
+#define VIAL_COMBO_ENTRIES 6
 #define COMBO_TERM 30
