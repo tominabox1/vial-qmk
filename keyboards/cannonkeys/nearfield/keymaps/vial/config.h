85f8cd47c837394818a7d0076470f08694625b5e
TJ <tom.campie@gmail.com>
2022-08-29 19:27:11 -0500

    vial compat

---
+++ keyboards/cannonkeys/nearfield/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6E, 0x72, 0x19, 0x99, 0x3D, 0x6C, 0x1D, 0xD9}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
+
+#define VIAL_TAP_DANCE_ENTRIES 4
