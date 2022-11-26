6a8304de31a82243238016749d6763dab82d6cd2
TJ Campie <tom.campie@gmail.com>
2022-11-26 10:18:30 -0600

    initial

---
+++ keyboards/danslayout/config.h
@@ -0,0 +1,39 @@
+/* Copyright 2022 tominabox1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x7431
+#define PRODUCT_ID      0x646C
+#define DEVICE_VER      0x0001
+#define PRODUCT         DansKeyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { B6, F6, F7, E6, B0, B1 }
+#define MATRIX_COL_PINS { B7, C6, C7, B3, B2, D0, D1, D2, D3, D5, B5, B4, D7, D6, D4 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
